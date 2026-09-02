a1d1b5fc2e38b2e23bf2368419154ab0d96e2e11
randall-zapata <randallzapata@gmail.com>
2026-09-02 18:53:33 -0400

    Add migrate-unifi-firewalla.sh v1.11

---
+++ migrate-unifi-firewalla.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# migrate-unifi-firewalla.sh (v1.11)
+# See header in --help. Never deletes /data/unifi or jacobalberty image.
+set -u -o pipefail
+COMPOSE_DIR=/home/pi/.firewalla/run/docker/unifi
+# Full script is in the companion download; this commit placeholder will be replaced.
+# Run: bash migrate-unifi-firewalla.sh --help
+echo "Use the v1.11 script from the repo raw URL after the full file lands."
+exit 2
